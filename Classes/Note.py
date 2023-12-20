82af19debc43b0c29788d6fa63fe60a6ed40a4e8
mal4ishka <nesterovvlad@gmail.com>
2023-12-20 23:45:14 +0200

    updated notes

--- Classes/Note.py
+++ Classes/Note.py
@@ -1,87 +1,76 @@
 class Note:
-    def __init__(self, text, tags):
+    def __init__(self, text, tags=None):
         self.text = text
         self.tags = tags
+
+    def __str__(self):
+        if self.tags == None:
+            self.tags = ''
+        return f"Text: {self.text}, tags: {self.tags}"
 
 
 class PersonalNoteAssistant:
     def __init__(self):
         self.notes = []
 
-    def add_note(self, text, tags):
-        note = Note(text, tags)
-        self.notes.append(note)
+    def add_note(self):
+        text = input(f"Enter a new note text: ")
+        wait_for_tags = input(f"Do you like to add some tags? (y/n)? ")
+        while True:
+            if wait_for_tags.casefold() == 'y':
+                tags = input('Enter tags (comma separated): ')
+                note = Note(text, tags)
+                self.notes.append(note)
+                break
+            elif wait_for_tags.casefold() == 'n':
+                note = Note(text)
+                self.notes.append(note)
+                break
+            elif wait_for_tags.casefold() != 'y' or wait_for_tags.casefold() != 'n':
+                wait_for_tags = input('Please type y or n: ')
+        print("Note was successfully added")
 
-    def edit_note(self, index, new_text, new_tags):
-        if 0 <= index < len(self.notes):
-            self.notes[index].text = new_text
-            self.notes[index].tags = new_tags
-        else:
-            print("Invalid index")
+    def edit_note(self):
+        print(f"Here's a list of your notes:")
+        self.print_notes()
+        while True:
+            index = int(input('Enter the index of the note you want to edit: '))
+            if index < 1 or index > len(self.notes):
+                print(f'Index is out of the range. You have to enter index in the range of [1 - {len(self.notes)}]')
+            elif 1 <= index <= len(self.notes):
+                new_text = input('Enter the new text for the note: ')
+                wait_for_tags = input(f"Do you like to modify the tags? (y/n)? ")
+                if wait_for_tags.casefold() == 'y':
+                    tags = input('Enter tags (comma separated): ')
+                    new_note = Note(new_text, tags)
+                    self.notes[index - 1] = new_note
+                    print("Note was successfully modified")
+                    break
+                elif wait_for_tags.casefold() == 'n':
+                    old_tags = self.notes[index - 1].tags
+                    new_note = Note(new_text, old_tags)
+                    self.notes[index - 1] = new_note
+                    print("Note was successfully modified")
+                    break
+                elif wait_for_tags.casefold() != 'y' or wait_for_tags.casefold() != 'n':
+                    wait_for_tags = input('Please type y or n: ')
 
-    def delete_note(self, index):
-        if 0 <= index < len(self.notes):
-            del self.notes[index]
-        else:
-            print("Invalid index")
+    def delete_note(self):
+        print(f"Here's a list of your notes:")
+        self.print_notes()
+        while True:
+            index = int(input('Enter the index of the note you want to delete: '))
+            if index < 1 or index > len(self.notes):
+                print(f'Index is out of the range. You have to enter index in the range of [1 - {len(self.notes)}]')
+            elif 1 <= index <= len(self.notes):
+                del self.notes[index - 1]
+                print(f'Note was successfully deleted')
+                break
 
     def search_notes_by_tag(self, tag):
         matching_notes = [note for note in self.notes if tag in note.tags]
         return matching_notes
 
-    def display_notes(self):
-        for i, note in enumerate(self.notes):
-            print(f"Note {i + 1}:")
-            print(f"Text: {note.text}")
-            print(f"Tags: {', '.join(note.tags)}")
-            print("---------------")
-
-    #  create personal assistant"
-
-    # assistant = PersonalAssistant()
-
-    def note_processing(self):
-
-        print("\n Please enter your choice!")
-        print("1. Add note")
-        print("2. Modify note")
-        print("3. Delete note")
-        print("4. Search note by tag")
-        print("5. Print all notes")
-        print("6. Exit")
-
-        choice = input("Select your option: ")
-
-        if choice == "1":
-            text = input("Input note: ")
-            tags = input("Input tags separated by comma: ").split(", ")
-            self.add_note(text, tags)
-
-        elif choice == "2":
-            index = int(input("Enter the note number for edit: ")) - 1
-            new_text = input("Enter the new text for the note: ")
-            new_tags = input("Enter all tags separated by comma: ").split(", ")
-            self.edit_note(index, new_text, new_tags)
-
-        elif choice == "3":
-            index = int(input("Enter the number of the note to delete: ")) - 1
-            self.delete_note(index)
-
-        elif choice == "4":
-            tag = input("Input tag for the search: ")
-            matching_notes = self.search_notes_by_tag(tag)
-            if matching_notes:
-                print("Notes found:")
-                for note in matching_notes:
-                    print(f"Text: {note.text}")
-                    print(f"Tags: {', '.join(note.tags)}")
-            else:
-                print("Note with this tag is not found.")
-
-        elif choice == "5":
-            self.display_notes()
-
-        elif choice == "6":
-            pass
-        else:
-            print("Incorrect input. Try again!")
+    def print_notes(self):
+        for note in self.notes:
+            print(note)
--- input_handler/handler.py
+++ input_handler/handler.py
@@ -22,60 +22,76 @@ def parser(inquire):
     if inquire == 'add_contact':
         record = Record()
         address_book.add_contact(record)
-        make_dump()
+        make_dump(book_name, address_book)
     elif inquire == 'print_contacts':
         address_book.print_contacts()
     elif inquire == 'add_email':
         address_book.add_email()
-        make_dump()
+        make_dump(book_name, address_book)
     elif inquire == 'add_birthday':
         address_book.add_birthday()
-        make_dump()
+        make_dump(book_name, address_book)
     elif inquire == 'add_phone':
         address_book.add_phone()
-        make_dump()
+        make_dump(book_name, address_book)
     elif inquire == 'delete_phone':
         address_book.delete_phone()
-        make_dump()
+        make_dump(book_name, address_book)
     elif inquire == 'delete_contact':
         address_book.delete_contact()
-        make_dump()
+        make_dump(book_name, address_book)
     elif inquire == 'search_contacts':
         address_book.search_contacts()
-    elif inquire == 'add_note':
-        address_book.add_note()
-        make_dump()
-    elif inquire == 'delete_note':
-        address_book.delete_note()
-        make_dump()
     elif inquire == 'delete_email':
         address_book.delete_email()
-        make_dump()
+        make_dump(book_name, address_book)
     elif inquire == 'edit_name':
         address_book.edit_name()
-        make_dump()
+        make_dump(book_name, address_book)
     elif inquire == 'search_birthdays':
         address_book.search_birthdays()
     elif inquire == 'sort_files':
         sorter = FileSorted()
         sorter.clean_folder()
+    elif inquire == 'add_note':
+        notes.add_note()
+        make_dump(notes_name, notes)
+    elif inquire == 'print_notes':
+        notes.print_notes()
+    elif inquire == 'delete_note':
+        notes.delete_note()
+        make_dump(notes_name, notes)
+    elif inquire == 'edit_note':
+        notes.edit_note()
+        make_dump(notes_name, notes)
     else:
         print('Something went wrong. Please try again')
 
 
-def make_dump():
-    with open(data_name, "wb") as f:
-        pickle.dump(address_book, f)
+def make_dump(name, object):
+    with open(name, "wb") as f:
+        pickle.dump(object, f)
 
 
-data_name = 'data/data.bin'
-dats_size = os.path.getsize(data_name)
+book_name = 'data/book.bin'
+book_size = os.path.getsize(book_name)
 
-if dats_size == 0:
+if book_size == 0:
     address_book = AddressBook()
 else:
-    with open(data_name, "rb") as file:
-        address_book = pickle.load(file)
+    with open(book_name, "rb") as book_file:
+        address_book = pickle.load(book_file)
+
+
+notes_name = 'data/notes.bin'
+notes_size = os.path.getsize(notes_name)
+
+if notes_size == 0:
+    notes = PersonalNoteAssistant()
+else:
+    with open(notes_name, "rb") as notes_file:
+        notes = pickle.load(notes_file)
+
 
 with open('man.txt', 'r') as fh:
     man = fh.read()
